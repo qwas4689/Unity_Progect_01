80878eb0b97e64ab7e42b4f7812194ec6002749e
qwas4689 <103081497+qwas4689@users.noreply.github.com>
2022-08-01 19:30:37 +0900

    [save] 중간저장
    1. 대각선 다리 제작 중

--- My project/Assets/Script/Obstacles/Bridge/SavePointBridge.cs
+++ My project/Assets/Script/Obstacles/Bridge/SavePointBridge.cs
@@ -5,23 +5,53 @@ using UnityEngine;
 public class SavePointBridge : MonoBehaviour
 {
     public GameObject _target;
+    public GameObject _startTarget;
+
     private Rigidbody _rigidbody;
     private Vector3 SavePointMoveBridge;
-
+    Vector3 aaa;
+    public bool onoff;
     // Start is called before the first frame update
     void Start()
     {
+        onoff = false;
         _rigidbody = GetComponent<Rigidbody>();
 
         SavePointMoveBridge = _target.transform.position - transform.position;
 
-        SavePointMoveBridge.y = 0f;
+        // SavePointMoveBridge.y = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        _rigidbody.velocity = SavePointMoveBridge * 5f * Time.deltaTime;
+        if(onoff)
+        {
+        aaa = transform.position = Vector3.MoveTowards(_startTarget.transform.position, _target.transform.position, 10f);
+
+        }
+        else
+        {
+            aaa = transform.position = Vector3.MoveTowards(_target.transform.position,_startTarget.transform.position,  10f);
+        }
+        _rigidbody.velocity = aaa * 1f * Time.deltaTime;
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Platform")
+        {
+
+            onoff = !onoff;
+        }
+    }
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.tag == "Platform")
+    //    {
+            
+    //        onoff = !onoff;
+    //    }
+    //}
 }
